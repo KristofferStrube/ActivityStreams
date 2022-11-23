0a3543fda143c79eb8a93e0e0faf04933e43ec80
KristofferStrube <kristoffer.strube@gmail.com>
2022-11-23 16:34:40 +0100

    Added Example 131

--- src/KristofferStrube.ActivityStreams/LinkTypes/Link.cs
+++ src/KristofferStrube.ActivityStreams/LinkTypes/Link.cs
@@ -5,6 +5,17 @@ namespace KristofferStrube.ActivityStreams;
 
 public class Link : ObjectOrLink, ILink
 {
+    /// <summary>
+    /// The target resource pointed to by a Link.
+    /// </summary>
     [JsonPropertyName("href")]
     public Uri? Href { get; set; }
+
+    /// <summary>
+    /// A link relation associated with a Link.The value must conform to both the[HTML5] and[RFC5988] "link relation" definitions.
+    /// In the[HTML5], any string not containing the "space" U+0020, "tab" (U+0009), "LF" (U+000A), "FF" (U+000C), "CR" (U+000D) or "," (U+002C) characters can be used as a valid link relation.
+    /// </summary>
+    [JsonConverter(typeof(OneOrMultipleConverter<string>))]
+    [JsonPropertyName("rel")]
+    public IEnumerable<string>? Rel { get; set; }
 }
--- tests/KristofferStrube.ActivityStreams.Tests/LinkTests.cs
+++ tests/KristofferStrube.ActivityStreams.Tests/LinkTests.cs
@@ -24,5 +24,32 @@ public class LinkTests
         ex121.Should().BeAssignableTo<Link>();
         ex121.As<Link>().Href.Should().Be(new Uri("http://example.org/abc"));
     }
+
+    [Fact]
+    /// <remarks>Example 131 taken from https://www.w3.org/TR/activitystreams-vocabulary/#dfn-rel</remarks>
+    public void Example_131()
+    {
+        // Arrange
+        var input = """
+            {
+              "@context": "https://www.w3.org/ns/activitystreams",
+              "type": "Link",
+              "href": "http://example.org/abc",
+              "hreflang": "en",
+              "mediaType": "text/html",
+              "name": "Preview",
+              "rel": ["canonical", "preview"]
+            }
+            """;
+
+        // Act
+        var ex131 = Deserialize<IObjectOrLink>(input);
+
+        // Assert
+        ex131.Should().BeAssignableTo<Link>();
+        ex131.As<Link>().Rel.Should().HaveCount(2);
+        ex131.As<Link>().Rel.First().Should().Be("canonical");
+        ex131.As<Link>().Rel.Last().Should().Be("preview");
+    }
 }
 
