02d2c814d8ecc8b559c9393e03be84eb2cea61cc
KristofferStrube <kristoffer.strube@gmail.com>
2022-11-23 16:24:07 +0100

    Added Example 121

---
+++ tests/KristofferStrube.ActivityStreams.Tests/LinkTests.cs
@@ -0,0 +1,28 @@
+﻿namespace KristofferStrube.ActivityStreams.Tests;
+
+public class LinkTests
+{
+    [Fact]
+    /// <remarks>Example 121 taken from https://www.w3.org/TR/activitystreams-vocabulary/#dfn-href</remarks>
+    public void Example_121()
+    {
+        // Arrange
+        var input = """
+            {
+              "@context": "https://www.w3.org/ns/activitystreams",
+              "type": "Link",
+              "href": "http://example.org/abc",
+              "mediaType": "text/html",
+              "name": "Previous"
+            }
+            """;
+
+        // Act
+        var ex121 = Deserialize<IObjectOrLink>(input);
+
+        // Assert
+        ex121.Should().BeAssignableTo<Link>();
+        ex121.As<Link>().Href.Should().Be(new Uri("http://example.org/abc"));
+    }
+}
+
