b58c3586cd319017c14f351861e97c21aafe454e
Gordon Pedersen <death.au@death.id.au>
2024-04-05 15:15:13 +1100

    Intransitive Activity inherits from Activity
    According to the spec, IntransitiveActivity "Inherits all properties from Activity except object."
    So, in order to achieve this (and, more selfishly to make it easier to work with Intransitive Activities alongside other Activities) I changed it to inherit from Activity but "hide" the Object property.
    (By marking it `private new` and `[Obsolete]`)

--- src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
+++ src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
@@ -3,47 +3,13 @@ using System.Text.Json.Serialization;
 
 namespace KristofferStrube.ActivityStreams;
 
-public class IntransitiveActivity : Object
+public class IntransitiveActivity : Activity
 {
     public IntransitiveActivity() => Type = new List<string>() { "IntransitiveActivity" };
 
-    /// <summary>
-    /// Describes one or more entities that either performed or are expected to perform the activity. Any single activity can have multiple actors. The actor may be specified using an indirect Link.
-    /// </summary>
-    [JsonPropertyName("actor")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Actor { get; set; }
-
-    /// <summary>
-    /// Describes the indirect object, or target, of the activity. The precise meaning of the target is largely dependent on the type of action being described but will often be the object of the English preposition "to". For instance, in the activity "John added a movie to his wishlist", the target of the activity is John's wishlist. An activity can have more than one target.
-    /// </summary>
-    [JsonPropertyName("target")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Target { get; set; }
-
-    /// <summary>
-    /// Describes the result of the activity. For instance, if a particular action results in the creation of a new resource, the result property can be used to describe that new resource.
-    /// </summary>
-    [JsonPropertyName("result")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Result { get; set; }
-
-    /// <summary>
-    /// Describes an indirect object of the activity from which the activity is directed. The precise meaning of the origin is the object of the English preposition "from". For instance, in the activity "John moved an item to List B from List A", the origin of the activity is "List A".
-    /// </summary>
-    [JsonPropertyName("origin")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Origin { get; set; }
-
-    /// <summary>
-    /// Identifies one or more objects used (or to be used) in the completion of an Activity.
-    /// </summary>
-    [JsonPropertyName("instrument")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Instrument { get; set; }
+  /// <summary>
+  /// Instances of IntransitiveActivity are a subtype of Activity representing intransitive actions. The object property is therefore inappropriate for these activities. 
+  /// </summary>
+  [Obsolete("The object property is inappropriate for intransitive activities.", true)]
+  private new IEnumerable<IObjectOrLink>? Object { get => base.Object; set => base.Object = value; }
 }
