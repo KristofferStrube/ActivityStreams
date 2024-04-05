738414e3085e4a2e6cdfe220073fa34ce6ece8cb
Gordon Pedersen <death.au@death.id.au>
2024-04-05 14:56:56 +1100

    Intransitive Activities do not have Objects
    As per https://www.w3.org/TR/activitystreams-vocabulary/#dfn-intransitiveactivity

--- src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
+++ src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
@@ -16,14 +16,6 @@ public class IntransitiveActivity : Object
     public IEnumerable<IObjectOrLink>? Actor { get; set; }
 
     /// <summary>
-    /// Describes an object of any kind. The Object type serves as the base type for most of the other kinds of objects defined in the Activity Vocabulary, including other Core types such as Activity, IntransitiveActivity, Collection and OrderedCollection.
-    /// </summary>
-    [JsonPropertyName("object")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    [JsonConverter(typeof(OneOrMultipleConverter<IObjectOrLink>))]
-    public IEnumerable<IObjectOrLink>? Object { get; set; }
-
-    /// <summary>
     /// Describes the indirect object, or target, of the activity. The precise meaning of the target is largely dependent on the type of action being described but will often be the object of the English preposition "to". For instance, in the activity "John added a movie to his wishlist", the target of the activity is John's wishlist. An activity can have more than one target.
     /// </summary>
     [JsonPropertyName("target")]
