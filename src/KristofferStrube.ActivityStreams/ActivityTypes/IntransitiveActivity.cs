e2aa651723bc7e9fc5c3399ad3ec9f2aeaa328e4
Gordon Pedersen <death.au+github@death.id.au>
2024-04-08 11:21:08 +1000

    Update IntransitiveActivity.cs
    Changed spaces to tabs

--- src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
+++ src/KristofferStrube.ActivityStreams/ActivityTypes/IntransitiveActivity.cs
@@ -5,11 +5,11 @@ namespace KristofferStrube.ActivityStreams;
 
 public class IntransitiveActivity : Activity
 {
-    public IntransitiveActivity() => Type = new List<string>() { "IntransitiveActivity" };
-
-  /// <summary>
-  /// Instances of IntransitiveActivity are a subtype of Activity representing intransitive actions. The object property is therefore inappropriate for these activities. 
-  /// </summary>
-  [Obsolete("The object property is inappropriate for intransitive activities.", true)]
-  private new IEnumerable<IObjectOrLink>? Object { get => base.Object; set => base.Object = value; }
+	public IntransitiveActivity() => Type = new List<string>() { "IntransitiveActivity" };
+	
+ 	/// <summary>
+	/// Instances of IntransitiveActivity are a subtype of Activity representing intransitive actions. The object property is therefore inappropriate for these activities. 
+	/// </summary>
+	[Obsolete("The object property is inappropriate for intransitive activities.", true)]
+	private new IEnumerable<IObjectOrLink>? Object { get => base.Object; set => base.Object = value; }
 }
